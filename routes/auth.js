3085303e92ac735b2a2e370e8802591b37aa41c0
Anis Ahmad <anisahmad1770@gmail.com>
2024-11-13 23:07:11 +0500

    logout

--- routes/auth.js
+++ routes/auth.js
@@ -17,12 +17,14 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 (req,res)=>{
     res.redirect('/dashboard')
 })
-// @desc    Logout User
+// @desc    Logout user
 // @route   /auth/logout
-router.get('/logout', (req,res)=>{
-    req.logout()
-    res.redirect('/')
-})
+router.get('/logout', (req, res, next) => {
+    req.logout((error) => {
+        if (error) {return next(error)}
+        res.redirect('/')
+    })
+  })
 
 // Export the router to be used in other parts of the application
 module.exports = router
